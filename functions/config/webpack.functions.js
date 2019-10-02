e62094aaead60fedcf9331c710c053100f87bd21
Rob Bishop <1128396+robbishop@users.noreply.github.com>
2019-10-02 18:22:09 +0100

    serverless-http express

---
+++ functions/config/webpack.functions.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line
+const webpack = require('webpack');
+
+module.exports = {
+  // plugins: [new webpack.DefinePlugin({ 'global.GENTLY': false })],
+  // externals: [{ express: { commonjs: 'express' } }],
+};
--- functions/src/server.js
+++ functions/src/server.js
@@ -1,6 +1,20 @@
-exports.handler = function handler(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello, from Netlify Lambda Function',
-  });
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello, from Netlify Lambda Function');
+});
+
+app.use('/.netlify/functions/server', router);
+
+// Export lambda handler
+const handler = serverless(app);
+exports.handler = async (event, context) => {
+  // you can do other things here
+  const result = await handler(event, context);
+  // and here
+  return result;
 };
